29100cc991c60126dc1995cdd10d781e478bf017
Andy Davidson <andy@nosignal.org>
2015-04-10 19:00:50 +0100

    test can log into virtual router

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,8 @@
 import sys, argparse
+from jnpr.junos import Device
+
 sys.path.append('/home/andy/src/pypeer/lib')
 sys.path.append('/Users/andy/src/pypeer/lib')
-
 from pypeer.ConfigDictionary import ConfigDictionary
 
 def test_username():
@@ -12,3 +13,12 @@ def test_username():
 def test_can_find_rtr1():
 	config = ConfigDictionary('/Users/andy/src/pypeer/etc/example.ini')
 	assert config.get_router_ip('rtr1') == '91.194.69.4'
+
+def test_can_login_parse_serial():
+	config = ConfigDictionary('/Users/andy/src/pypeer/etc/example.ini')
+	jdev = Device(user=config.username(), host=config.get_router_ip('rtr1'), password=config.password())
+	jdev.open(gather_facts=False)
+	inv = jdev.rpc.get_chassis_inventory()
+	print "model: %s" % inv.findtext('.//chassis/description')
+	jdev.close()
+	assert inv.findtext('.//chassis/description') == 'VMX'
