d861d200d3839fa9046c16afc9b2b3bc33cb484b
Andy Davidson <andy@nosignal.org>
2015-04-10 09:42:36 +0100

    can read router ip from config file.

--- lib/pypeer/ConfigDictionary.py
+++ lib/pypeer/ConfigDictionary.py
@@ -5,6 +5,13 @@ class ConfigDictionary:
 	def __init__(self, configfile = '/home/andy/etc/pypeer.ini'):
 		self.localconfig = ConfigParser.RawConfigParser()
 		self.localconfig.read(configfile)
+		self.router_ip_dict  = {}
+		self.router_loc_dict = {}
+		router_items = self.localconfig.items("routers")
+		for router, payload in router_items:
+			(ip_addr, location) = payload.split(",")
+			self.router_ip_dict[router] = ip_addr
+			self.router_loc_dict[router] = location
 
 	def username(self):
 		return self.localconfig.get("auth", "username")
@@ -12,3 +19,5 @@ class ConfigDictionary:
 	def password(self):
 		return self.localconfig.get("auth", "password")
 
+	def get_router_ip(self, router_name):
+		return self.router_ip_dict[router_name]
