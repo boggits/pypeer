76240a3ee3f4e52a02603262f55e5061e56895fc
Andy Davidson <andy@nosignal.org>
2015-04-15 12:59:31 +0200

    Can now read prefix from route xml

--- lib/pypeer/RouteData.py
+++ lib/pypeer/RouteData.py
@@ -3,3 +3,6 @@
 class RouteData:
 	def __init__(self, routexml):
 		self.routexml = routexml
+
+	def prefix(self):
+		return self.routexml.find('rt-destination').text + "/" + self.routexml.find('rt-prefix-length').text
--- tests/test_offline.py
+++ tests/test_offline.py
@@ -1,9 +1,12 @@
 import sys, argparse
+import os
 from jnpr.junos import Device
+from lxml import etree
 
 sys.path.append('/home/andy/src/pypeer/lib')
 sys.path.append('/Users/andy/src/pypeer/lib')
 from pypeer.ConfigDictionary import ConfigDictionary
+from pypeer.RouteData import RouteData
 
 def test_username():
 	config = ConfigDictionary('/Users/andy/src/pypeer/etc/example.ini')
@@ -14,4 +17,7 @@ def test_can_find_rtr1():
 	config = ConfigDictionary('/Users/andy/src/pypeer/etc/example.ini')
 	assert config.get_router_ip('rtr1') == '91.194.69.4'
 
-
+def test_can_read_prefix_from_route_object():
+	resultxml = etree.fromstring(open('/Users/andy/src/pypeer/tests/test_data/bgp_route.xml').read())
+	route = RouteData(resultxml.find('.//rt'))
+	assert route.prefix() == '199.87.242.0/24'
