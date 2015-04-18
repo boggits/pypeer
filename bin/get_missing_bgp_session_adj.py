b2eb4b0105601d124f7ac7fc7985eff3cda22cc5
James Jeffries <jamesjeffries1@gmail.com>
2015-04-18 16:05:12 +0100

    refactoring

--- bin/get_missing_bgp_session_adj.py
+++ bin/get_missing_bgp_session_adj.py
@@ -28,54 +28,55 @@ def is_valid_ipv4_address(address):
         return False
 
     return True
+def main(peer_asn):
+    list_peering_ips_of_target_asn = []
 
+    config = ConfigDictionary()
+    username = config.username()
+    password = config.password()
 
-config = ConfigDictionary()
-username = config.username()
-password = config.password()
+    for router in config.get_list_of_router_names():
+        jdev = Device(user=username, host=config.get_router_ip(router), password=password)
+        jdev.open(gather_facts=False)
+        jdev.timeout=600
 
-parser = argparse.ArgumentParser(description='Inspect a router estate and identify missing opportunities to peer with an asn')
-parser.add_argument('--asn', dest='bgppeer', help='bgp peer asn')
-args = parser.parse_args()
-peer_asn  = args.bgppeer
+        try:
+            resultxml = jdev.rpc.get_bgp_summary_information()
+        except Exception as err:
+            print "CMD:"   
+            etree.dump(err.cmd)   
+            print "RSP:"   
+            etree.dump(err.rsp)
 
-list_peering_ips_of_target_asn = []
+        bgpsum = BgpData(resultxml)
+        for thispeeringip in bgpsum.get_list_ipaddr_from_asn(peer_asn):
+            list_peering_ips_of_target_asn.append(thispeeringip)
 
-for router in config.get_list_of_router_names():
-	jdev = Device(user=username, host=config.get_router_ip(router), password=password)
-	jdev.open(gather_facts=False)
-	jdev.timeout=600
+    print "all peering ips : " + str(list_peering_ips_of_target_asn)
 
-	try:
-		resultxml = jdev.rpc.get_bgp_summary_information()
-	except Exception as err:
-		print "CMD:"   
-		etree.dump(err.cmd)   
-		print "RSP:"   
-		etree.dump(err.rsp)
+    exchange = Exchange()
+    list_sessions_configured_peeringdbid_exchanges_of_target_asn = []
+    for peering_ip in list_peering_ips_of_target_asn:
+        if not is_valid_ipv4_address(peering_ip):
+            next
+        else:
+            peeringdb_id = exchange.get_exchange_from_peerip(peering_ip)['peeringdbid']
+            list_sessions_configured_peeringdbid_exchanges_of_target_asn.append(peeringdb_id)
 
-	bgpsum = BgpData(resultxml)
-	for thispeeringip in bgpsum.get_list_ipaddr_from_asn(peer_asn):
-		list_peering_ips_of_target_asn.append(thispeeringip)
+    # todo: pull this from config file
+    list_my_exchanges = [26, 806, 87, 59, 33, 31, 804, 1, 255, 5, 359, 48, 387, 435, 583, 745, 18, 777, 53, 297, 35, 70, 64, 60, 325, 587]
 
-print "all peering ips : " + str(list_peering_ips_of_target_asn)
+    peeringdb = PeeringDBClient()
+    list_their_exchanges = peeringdb.get_list_connected_ixp(peer_asn)
 
-exchange = Exchange()
-list_sessions_configured_peeringdbid_exchanges_of_target_asn = []
-for peering_ip in list_peering_ips_of_target_asn:
-	if not is_valid_ipv4_address(peering_ip):
-		next
-	else:
-		peeringdb_id = exchange.get_exchange_from_peerip(peering_ip)['peeringdbid']
-		list_sessions_configured_peeringdbid_exchanges_of_target_asn.append(peeringdb_id)
+    mutual_exchanges = set(list_my_exchanges).intersection(list_their_exchanges)
+    missing_exchanges = set(mutual_exchanges).difference(list_sessions_configured_peeringdbid_exchanges_of_target_asn)
 
-# todo: pull this from config file
-list_my_exchanges = [26, 806, 87, 59, 33, 31, 804, 1, 255, 5, 359, 48, 387, 435, 583, 745, 18, 777, 53, 297, 35, 70, 64, 60, 325, 587]
+    print "Missing exchanges are:" + str(missing_exchanges)
 
-peeringdb = PeeringDBClient()
-list_their_exchanges = peeringdb.get_list_connected_ixp(peer_asn)
-
-mutual_exchanges = set(list_my_exchanges).intersection(list_their_exchanges)
-missing_exchanges = set(mutual_exchanges).difference(list_sessions_configured_peeringdbid_exchanges_of_target_asn)
-
-print "Missing exchanges are:" + str(missing_exchanges)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Inspect a router estate and identify missing opportunities to peer with an asn')
+    parser.add_argument('--asn', dest='bgppeer', help='bgp peer asn', require=True)
+    args = parser.parse_args()
+    peer_asn  = args.bgppeer
+    main(peer_asn)
