c522afbe2b4e8de4a7090586e94fa511d6d70898
Andy Davidson <andy@nosignal.org>
2015-04-21 11:56:57 +0100

    Can get a list of peering ips in a bgpsum

--- lib/pypeer/BgpData.py
+++ lib/pypeer/BgpData.py
@@ -2,6 +2,12 @@ class BgpData:
     def __init__(self, bgpsumxml):
         self.bgpxml = bgpsumxml
 
+    def get_list_peering_ips(self):
+        list_peer_ip = []
+        for bgp_sum in self.bgpxml.findall('.//bgp-peer'):
+            list_peer_ip.append(bgp_sum.find('peer-address').text)
+        return list_peer_ip
+
     def get_list_ipaddr_from_asn(self, asn):
         list_peer_ip = []
         for bgp_sum in self.bgpxml.findall('.//bgp-peer'):
